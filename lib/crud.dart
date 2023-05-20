7113b8984897cf241b8cf6d9584af1967a3c262c
Mazedul <mazedulislam4970@gmail.com>
2023-05-20 18:18:46 +0600

    GetX CRUD operation

---
+++ lib/crud.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CRUD extends StatefulWidget {
+  const CRUD({Key? key}) : super(key: key);
+
+  @override
+  State<CRUD> createState() => _CRUDState();
+}
+
+class _CRUDState extends State<CRUD> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
